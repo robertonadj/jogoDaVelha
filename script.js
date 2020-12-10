7396740a987f09bfdff2b5540e85d7ff8e7d7a01
Roberto Nadj <robertonadj@yahoo.com.br>
2020-12-10 17:22:16 -0300

    projeto com problema no video. Aguardando correcao

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
+var jogadorSelecionado = document.getElementById();
 var jogador = null;
 
 function escolherQuadrado(id) {
-    console.log('Clicou no botão');
+    var quadrado = document.getElementById(id);
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000';
+}
+
+function mudarJogador(valor) {
+    jogador = valor;
 }
